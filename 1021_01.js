3d613ccf4da3c39ac17dbf538d2bd0e35ea4571d
jeouneunlee <ryts59018@gmail.com>
2025-10-21 10:35:30 +0900

    new

---
+++ 1021_01.js
@@ -0,0 +1,4 @@
+const fruit = ["banana","melon","watermelon"]
+console.log(fruit[0]);
+console.log(fruit[1]);
+console.log(fruit[2]);
---
+++ 1021_02.js
@@ -0,0 +1,20 @@
+// animals라는 변수에 동물이름을 5가지 입력하여 배열변수를 만들고, 동물이름을 출력하시오
+const animals = ['kangeroo','cat','puppy','panda',"sloth"]
+console.log(animals[2]);
+
+for (const animal of animals) {
+    console.log(animal);
+}
+
+/*
+for(초기화 ; 조건식 ; 증감식) {
+    실행문
+} */
+
+    //0 부터 5까지를 출력한다.
+
+for(let i=0; i< 5; i++) {
+    let print = `${i + 1}. ${animals[i]}`;
+    console.log(print);
+}
+
