b72f862527d7bcea0db7156444f668cc94bcf617
jeouneunlee <ryts59018@gmail.com>
2025-10-24 16:39:39 +0900

    modify

--- 1024.js
+++ 1024.js
@@ -1,6 +1,7 @@
 const numbers = [32,20,5,12,0,45]
 const str = 'Hello, World'
 let result = 0
+let count = 0
 
 //for문을 이용해서 numbers의 모든 숫자를 console에 찍기
 
@@ -12,26 +13,38 @@ for(let i=0; i< numbers.length; i++) {
 }
 
 
-//4번 문제
+ //1번 문제
 for(let i=0; i< numbers.length; i++) {
-    result = result + numbers[i];}
-    console.log(`${numbers}의 총 합은 ${result}입니다.`);
-
-
-//3번 문제
-for(let i=0; i< numbers.length; i++) {
-    if (numbers[i]>= 20)
-    console.log(`${numbers[i]} 은 20보다 크다`)
-}
-
+    if (numbers[i]%2 === 1)
+    console.log(`${numbers[i]} 는 홀수`)
+}   
 
 //2번 문제
 console.log(str.length)
 
 
-//1번 문제
+//3번 문제
+
 for(let i=0; i< numbers.length; i++) {
-    if (numbers[i]%2 === 1)
-    console.log(`${numbers[i]} 는 홀수`)
+    result = result + numbers[i];}
+    console.log(`${numbers}의 총 합은 ${result}입니다.`);
+
+
+//4번 문제
+
+for (const number of numbers) {
+    if (number>=20)
+        console.log(number)
 }
+
+
+for(let i=0; i< numbers.length; i++) {
+    if (numbers[i]>=20){
+    count++;
+}}
+
+    
+console.log(count);
+
+
 
