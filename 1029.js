9692e2155deebd1ecedea297cfc31dbd1076a0c7
jeouneunlee <ryts59018@gmail.com>
2025-10-29 09:17:16 +0900

    new

---
+++ 1029.js
@@ -0,0 +1,13 @@
+const users = ["irangi","camel","ghost","sunny","henry"]
+let userIndex = 3;
+
+users.forEach(funciton (users,idx)) 
+  if (idx === 2) {
+console.log(`${idx+1}번쩨 user는 ${users}님 입니다.`)};
+
+users.forEach((users,idx)=> {
+    if (idx === userIndex - 1) {
+console.log(`${userIndex+1}번쩨 user는 ${users}님 입니다.`);
+    }
+
+});
