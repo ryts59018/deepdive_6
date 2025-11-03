db1aed7b15e7d7cd93d07b8339ee7e45281c8e0f
jeouneunlee <ryts59018@gmail.com>
2025-11-03 09:50:55 +0900

    practice

---
+++ 1103.js
@@ -0,0 +1,15 @@
+// 공배수 구하기
+const result = 0
+function getNumber(num, x){
+if (num % x = 0) {
+     result + 1
+} else (num % x = 1){
+    result
+}
+ 
+
+
+}
+
+getNumber(60, 2) // 뒤에 있는 x가 앞에있는 num의 공배수이면 1 출력, 아니면 0을 출력
+getNumber(60, 7)
